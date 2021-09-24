6341adfcc7b03737d88f6fe319ac18582999dcc2
Fraser <Fraser.Hughes@btinternet.com>
2021-09-24 14:30:20 +0200

    Created tests and functionality for not having authentication. Installed the Devise Gem.

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,4 +1,6 @@
 class Api::ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: :create
+
   def index
     articles = Article.all
     if articles.any?
@@ -7,4 +9,19 @@ class Api::ArticlesController < ApplicationController
       render json: { message: 'There are no articles' }
     end
   end
+
+  def create
+    # binding.pry
+    article = Article.create(title: params[:article][:title],
+                             lede: params[:article][:lede],
+                             author: params[:article][:author])
+    render json: { message: 'Your article was created' }, status: 201
+  end
+
+  private
+
+  # def authenicate_user!
+  #   #temp solution
+  #   render json: { message: 'You have to login or sign up first' }, status: 401
+  # end
 end
